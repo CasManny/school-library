3cb56bbf4257e0e03d5eefb6bf6128e5d11ff5cc
CasManny <chukwusabastine2018@gmail.com>
2025-01-15 23:19:57 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const ibmPlexsans = localFont({
+  src: [
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
+});
+
+const bebasNeue = localFont({
+  src: [
+    { path: '/fonts/BebasNeue-Regular.ttf', weight: "400", style: "normal" },
+  ],
+  variable: "--bebas-neue"
+})
+
+export const metadata: Metadata = {
+  title: "BookWise",
+  description: "A book borrowing university library management solution",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexsans.className} ${bebasNeue.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
