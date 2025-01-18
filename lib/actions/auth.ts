19e50200d880cac8e39315b4464eb4f10ed521f6
CasManny <chukwusabastine2018@gmail.com>
2025-01-18 23:47:53 +0100

    implement upstash rate limiting

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -5,51 +5,70 @@ import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import ratelimit from "../rate-limit";
+import { redirect } from "next/navigation";
 
 export const signup = async (params: AuthCredentials) => {
   const { fullName, email, universityCard, universityId, password } = params;
+
+  // get user IP address
+    const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1"
+    const { success } = await ratelimit.limit(ip)
+    if(!success) return redirect('/too-fast')
   // check if the user already exists
   const existingUser = await db
     .select()
     .from(users)
     .where(eq(users.email, email))
-      .limit(1);
-    
-    if (existingUser.length > 0) {
-        return { success: false, error: "User already exists"}
-    }
+    .limit(1);
 
-    const hashedPassword = await hash(password, 10)
+  if (existingUser.length > 0) {
+    return { success: false, error: "User already exists" };
+  }
 
-    try {
-        await db.insert(users).values({
-            fullName,
-            email,
-            universityId,
-            password: hashedPassword,
-            universityCard
-        })
+  const hashedPassword = await hash(password, 10);
 
-        await signInWithCredential({email, password})
+  try {
+    await db.insert(users).values({
+      fullName,
+      email,
+      universityId,
+      password: hashedPassword,
+      universityCard,
+    });
 
-        return { success: true}
-    } catch (error) {
-        console.log(error, 'signup error')
-        return { success: false, error: "Signup error"}
-    }
+    await signInWithCredential({ email, password });
+
+    return { success: true };
+  } catch (error) {
+    console.log(error, "signup error");
+    return { success: false, error: "Signup error" };
+  }
 };
 
-export const signInWithCredential = async (params: Pick<AuthCredentials, 'email' | 'password'>) => {
-    const { email, password} = params
-    try {
-        const result = await signIn('credentials', { email, password, redirect: false })
-        if (result?.error) {
-            return { success: false, error: result.error}
-        }
-
-        return { success: true}
-    } catch (error) {
-        console.log(error)
-        return { success: false, error: "signin error"}
+export const signInWithCredential = async (
+  params: Pick<AuthCredentials, "email" | "password">
+) => {
+    const { email, password } = params;
+    
+    // get user IP address
+    const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1"
+    const { success } = await ratelimit.limit(ip)
+    if(!success) return redirect('/too-fast')
+  try {
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    if (result?.error) {
+      return { success: false, error: result.error };
     }
-}
+
+    return { success: true };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: "signin error" };
+  }
+};
